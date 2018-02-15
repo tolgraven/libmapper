bff96c358f66315ab5adb50ed8b369e34a9cd29f
Joen Tolgraven <joen.tolgraven@gmail.com>
2018-02-15 21:29:22 +0100

    Fixed tkgui example

--- swig/tkgui.py
+++ swig/tkgui.py
@@ -12,8 +12,8 @@ def on_mapper_change(sig, id, x, timetag):
 
 dev = mapper.device("tkgui", 9000)
 
-sig_in = dev.add_input("/signal0", 1, 'i', None, 0, 100, on_mapper_change)
-sig_out = dev.add_output("/signal0", 1, 'i', None, 0, 100)
+sig_in = dev.add_input_signal("/signal0", 1, 'i', None, 0, 100, on_mapper_change)
+sig_out = dev.add_output_signal("/signal0", 1, 'i', None, 0, 100)
 
 master = Tkinter.Tk()
 master.title("libmapper Python GUI demo")
@@ -32,7 +32,7 @@ w.pack()
 def do_poll():
     global name_known
     dev.poll(20)
-    if dev.ready() and not name_known:
+    if dev.ready and not name_known:
         name.set('Device name: %s, listening on port %d'%(dev.name, dev.port))
         name_known = True
     master.after(5, do_poll)
